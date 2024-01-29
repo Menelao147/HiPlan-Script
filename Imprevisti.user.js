6425446a09168239ea9ec9c72cafc008c58e756e
Menelao147 <59983324+Menelao147@users.noreply.github.com>
2024-01-29 16:27:26 +0100

    Create Imprevisti.user.js

---
+++ Imprevisti.user.js
@@ -0,0 +1,62 @@
+// ==UserScript==
+// @name         Imprevisti
+// @namespace    http://tampermonkey.net/
+// @version      0.1
+// @author       Menelao147
+// @match        https://hiplan.sidel.com/HiPlan/HiPlan/Imprevisti/Imprevisti.phtml
+// @icon         https://hiplan.sidel.com/HiPlan/favicon.ico
+// @grant        none
+// ==/UserScript==
+
+(function() {
+    'use strict';
+    var i
+
+    let DataInput = document.getElementsByName('Dadatalav');
+    let ImprevistiList = document.getElementsByName('idriga');
+    let ImprevistiOre = document.getElementsByName('numore');
+    let ImprevistiMinuti = document.getElementsByName('nummin');
+
+    for (i = 0; i < ImprevistiList[0].childElementCount;) {
+        if (ImprevistiList[0].children[i].text == "Assenza") {
+            i++;
+        }
+        else if (ImprevistiList[0].children[i].text == "Seleziona") {
+            i++;
+        }
+        else if (ImprevistiList[0].children[i].text.includes("12320")) {
+            i++;
+        }
+        else if (ImprevistiList[0].children[i].text.includes("FLFIELD")) {
+            i++;
+        }
+        else if (ImprevistiList[0].children[i].text.includes("FLSERVICE")) {
+            i++;
+        }
+        else {
+            ImprevistiList[0].removeChild(ImprevistiList[0].children[i]);
+        }
+    }
+
+    for (i = 0; i < ImprevistiOre[0].childElementCount;) {
+        if (Number(ImprevistiOre[0].children[i].text) < 14) {
+            i++;
+        }
+        else {
+            ImprevistiOre[0].removeChild(ImprevistiOre[0].children[i]);
+        }
+    }
+
+    for (i = 0; i < ImprevistiMinuti[0].childElementCount;) {
+        if (Number(ImprevistiMinuti[0].children[i].text) == 0) {
+            i++;
+        }
+        else if (Number(ImprevistiMinuti[0].children[i].text) == 30) {
+            i++;
+        }
+        else {
+            ImprevistiMinuti[0].removeChild(ImprevistiMinuti[0].children[i]);
+        }
+    }
+})
+();
