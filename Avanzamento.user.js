f101425cbbb6a8ba0a8dc48012005417583a2a51
Menelao147 <59983324+Menelao147@users.noreply.github.com>
2024-10-24 18:12:00 +0200

    Update Avanzamento.user.js

--- Avanzamento.user.js
+++ Avanzamento.user.js
@@ -1,14 +1,15 @@
 // ==UserScript==
 // @name         Avanzamento
 // @namespace    http://tampermonkey.net/
-// @version      0.9
+// @version      1.0
 // @description  HiPlan Avanzamento
 // @author       Menelao147
-// @match        https://hiplan.sidel.com/HiPlan/HiPlan/avanzamento.phtml*
+// @match        */HiPlan/HiPlan/*vanzamento.phtml*
 // @icon         https://hiplan.sidel.com/HiPlan/favicon.ico
 // @grant        GM_setValue
 // @grant        GM_getValue
 // @require      https://github.com/Menelao147/HiPlan-Script/raw/main/GM_config.js
+// @require      https://github.com/Menelao147/HiPlan-Script/raw/main/moment.js
 // @downloadURL  https://github.com/Menelao147/HiPlan-Script/raw/main/Avanzamento.user.js
 // @updateURL    https://github.com/Menelao147/HiPlan-Script/raw/main/Avanzamento.user.js
 // ==/UserScript==
@@ -18,10 +19,11 @@ window.addEventListener('load', function() {
     window.moveTo(GM_getValue("WindowsPosX"), GM_getValue("WindowsPosY"));
     let i;
 
-    let DataAv = document.getElementsByName('Dadatlav')
+    let DataInputDa = document.getElementsByName('Dadatlav');
+    let DataInputA = document.getElementsByName('Adatlav');
     let DropOre = document.getElementsByName('numore');
     let DropMin = document.getElementsByName('nummin');
-    let Data = GM_getValue("DataAvanzamento").split("/");
+    let Data = GM_getValue("DataAvanzamento", Date()).split("/");
     let MeseAttuale = new Date().getMonth()
     let MaxDay = new Date(Data[2] + "/" + Data[1] + "/" + Data[0]).getDaysInMonth();
     let GiornoSettimana = new Date(Data[2] + "/" + Data[1] + "/" + Data[0]).getDay();
@@ -56,52 +58,58 @@ window.addEventListener('load', function() {
         if (parseInt(Data[0]) < 10){
             Data[0] = "0" + Data[0]
         }
-        DataAv[0].value = Data[0] + "/" + Data[1] + "/" + Data[2];
-        GM_setValue("DataAvanzamento", DataAv[0].value);
+        DataInputDa[0].value = Data[0] + "/" + Data[1] + "/" + Data[2];
+        GM_setValue("DataAvanzamento", DataInputDa[0].value);
     } else if (GiornoSettimana == 5 && (parseInt(Data[0]) + 3) <= MaxDay){
         Data[0] = parseInt(Data[0]) + 3
         if (parseInt(Data[0]) < 10){
             Data[0] = "0" + Data[0]
         }
-        DataAv[0].value = Data[0] + "/" + Data[1] + "/" + Data[2];
-        GM_setValue("DataAvanzamento", DataAv[0].value);
+        DataInputDa[0].value = Data[0] + "/" + Data[1] + "/" + Data[2];
+        GM_setValue("DataAvanzamento", DataInputDa[0].value);
     } else if (GiornoSettimana == 6 && (parseInt(Data[0]) + 2) <= MaxDay){
         Data[0] = parseInt(Data[0]) + 2
         if (parseInt(Data[0]) < 10){
             Data[0] = "0" + Data[0]
         }
-        DataAv[0].value = Data[0] + "/" + Data[1] + "/" + Data[2];
-        GM_setValue("DataAvanzamento", DataAv[0].value);
+        DataInputDa[0].value = Data[0] + "/" + Data[1] + "/" + Data[2];
+        GM_setValue("DataAvanzamento", DataInputDa[0].value);
     } else if ((parseInt(Data[0]) + 1) <= MaxDay){
         Data[0] = parseInt(Data[0]) + 1
         if (parseInt(Data[0]) < 10){
             Data[0] = "0" + Data[0]
         }
-        DataAv[0].value = Data[0] + "/" + Data[1] + "/" + Data[2];
-        GM_setValue("DataAvanzamento", DataAv[0].value);
+        DataInputDa[0].value = Data[0] + "/" + Data[1] + "/" + Data[2];
+        GM_setValue("DataAvanzamento", DataInputDa[0].value);
     } else if (MeseSalvato != MeseAttuale && (parseInt(Data[1]) + 1) <= 11) {
         Data[0] = "01"
         Data[1] = parseInt(Data[1]) + 1
         if (parseInt(Data[1]) < 10){
             Data[1] = "0" + Data[1]
         }
-        DataAv[0].value = Data[0] + "/" + Data[1] + "/" + Data[2];
-        GM_setValue("DataAvanzamento", DataAv[0].value);
+        DataInputDa[0].value = Data[0] + "/" + Data[1] + "/" + Data[2];
+        GM_setValue("DataAvanzamento", DataInputDa[0].value);
     } else {
         Data[0] = parseInt(Data[0])
-        DataAv[0].value = Data[0] + "/" + Data[1] + "/" + Data[2];
-        GM_setValue("DataAvanzamento", DataAv[0].value);
+        DataInputDa[0].value = Data[0] + "/" + Data[1] + "/" + Data[2];
+        GM_setValue("DataAvanzamento", DataInputDa[0].value);
     }
 }, false);
 
 window.addEventListener('input', function saveDate() {
-    let DataAv = document.getElementsByName('Dadatlav')
-    let Data = DataAv[0].value.split("/");
+    let DataInputDa = document.getElementsByName('Dadatlav')
+    let DataInputA = document.getElementsByName('Adatlav');
+    let Data = DataInputDa[0].value.split("/");
     if (Data[0].length > 1 && Data.length == 3) {
-        GM_setValue("DataAvanzamento", DataAv[0].value);
+        GM_setValue("DataAvanzamento", DataInputDa[0].value);
     }
     else if (parseInt(Data[0]) < 10 && Data.length == 3) {
         GM_setValue("DataAvanzamento", "0" + Data[0] + "/" + Data[1] + "/" + Data[2]);
+    }
+    if (event.target.name == 'Dadatlav') {
+        if (moment(DataInputDa[0].value, "DD/MM/YYYY", true).isValid()) {
+            DataInputA[0].value = DataInputDa[0].value;
+        }
     }
 }, false);
 
